b7e971c0be41bf89d40e993983dbd55f90767db7
Terry D'Entremont <tdentremont@cpac.ca>
2018-10-15 15:27:42 -0400

    concatenated javascript file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
 				gutil = require('gulp-util'),
 				coffee = require('gulp-coffee'),
-				coffeeSources = ['components/coffee/tagline.coffee'];
+				concat = require('gulp-concat');
+				
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var	jsSources = ['components/scripts/rclick.js',
+																	'components/scripts/pixgrid.js',
+																	'components/scripts/tagline.js',
+																	'componenet/scripts/template.js'];
 
 gulp.task('coffee', function(){
 				gulp.src( coffeeSources )
@@ -9,3 +15,9 @@ gulp.task('coffee', function(){
 						.on('error', gutil.log))
 				.pipe(gulp.dest('components/scripts'))		
 });
+
+gulp.task('js', function(){
+		gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+})
