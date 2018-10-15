fa036a83ff2e88369ec8defcb41ec565f8df21ad
Terry D'Entremont <tdentremont@cpac.ca>
2018-10-15 15:13:56 -0400

    added coffee script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-				gutil = require('gulp-util');
+				gutil = require('gulp-util'),
+				coffee = require('gulp-coffee'),
+				coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('default' , function(){
-	gutil.log('Workflows are awesome.');
+gulp.task('coffee', function(){
+				gulp.src( coffeeSources )
+				.pipe(coffee({ bare:true })
+						.on('error', gutil.log))
+				.pipe(gulp.dest('components/scripts'))		
 });
