3d1dfe4c4410263e542e1c4b1799b574f5948e4b
Terry D'Entremont <tdentremont@cpac.ca>
2018-10-16 12:11:22 -0400

    Added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var	jsSources = ['components/scripts/rclick.js',
 																	'components/scripts/tagline.js',
 																	'components/scripts/template.js'];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
 				gulp.src( coffeeSources )
@@ -44,13 +46,25 @@ gulp.task('watch',function(){
 		gulp.watch(coffeeSources, ['coffee']);
 		gulp.watch(jsSources, ['js']);
 		gulp.watch('components/sass/*.scss', ['compass']);
+		gulp.watch(htmlSources, ['html']);
+		gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default',['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default',['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
 gulp.task('connect', function(){
 		connect.server({
 				root: 'builds/development/',
 				livereload: true
 		});
+});
+
+gulp.task('html', function(){
+		gulp.src(htmlSources)
+		.pipe(connect.reload());
+});
+
+gulp.task('json', function(){
+		gulp.src(jsonSources)
+		.pipe(connect.reload());
 });
