5fa048f601621f58cb913361b485aeef3f1ecff0
Terry D'Entremont <tdentremont@cpac.ca>
2018-10-16 14:38:34 -0400

    crushed json file

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 				gulpif = require('gulp-if'),
 				uglify = require('gulp-uglify'),
 				minifyHTML = require('gulp-minify-html'),
+				jsonminify = require('gulp-jsonminify'),
 				concat = require('gulp-concat');
 
 var env, 
@@ -71,7 +72,7 @@ gulp.task('watch',function(){
 		gulp.watch(jsSources, ['js']);
 		gulp.watch('components/sass/*.scss', ['compass']);
 		gulp.watch('builds/development/*.html', ['html']);
-		gulp.watch(jsonSources, ['json']);
+		gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('default',['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
@@ -91,6 +92,8 @@ gulp.task('html', function(){
 });
 
 gulp.task('json', function(){
-		gulp.src(jsonSources)
+		gulp.src('builds/development/js/*.json')
+		.pipe(gulpif(env === 'production', jsonminify()))
+		.pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
 		.pipe(connect.reload());
 });
